ef249cc71822865533989d426b1d03714a06c5f1
simuneu <worksimu@gmail.com>
2025-09-27 23:26:49 +0900

    feat:user repository 추가

---
+++ src/main/java/com/bidnbuy/server/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.bidnbuy.server.repository;
+
+import com.bidnbuy.server.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+    UserEntity findByEmail(String email); //로그인, 정보 조회
+    Boolean existsByEmail(String email); //아이디(이메일) 중복확인
+}
