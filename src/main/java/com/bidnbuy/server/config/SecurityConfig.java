f16718776904e4b8f1fe3b1de0f07a39b35e9fc7
“pshingsl” <w455ok@gmail.com>
2025-09-29 18:28:16 +0900

    fix:시큐리티 임시로 경매등록을 위해 수정

--- src/main/java/com/bidnbuy/server/config/SecurityConfig.java
+++ src/main/java/com/bidnbuy/server/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.bidnbuy.server.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -28,6 +29,7 @@ public class SecurityConfig {
 
             .authorizeHttpRequests(authorize -> authorize
                     .requestMatchers("/auth/**").permitAll()
+                    .requestMatchers(HttpMethod.POST, "/auctions").permitAll()
                     .anyRequest().authenticated()
             );
 
