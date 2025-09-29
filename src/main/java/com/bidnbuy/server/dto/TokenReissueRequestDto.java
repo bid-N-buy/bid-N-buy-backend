ddaa50aa4d4ab611026589c43aa60b6b07ed39ac
simuneu <worksimu@gmail.com>
2025-09-29 19:32:30 +0900

    feat: refresh token , access token재발급

--- src/main/java/com/bidnbuy/server/dto/TokenReissueRequestDto.java
+++ src/main/java/com/bidnbuy/server/dto/TokenReissueRequestDto.java
@@ -1,11 +1,13 @@
 package com.bidnbuy.server.dto;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 //액세스 토큰 만료시 새 토큰을 발급받기 위해 (클라이어느에서 저장했던) 리프레시토큰값을 서버에 전달
 @Getter
-@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class TokenReissueRequestDto {
     private String refreshToken;
+    private String accessToken;
 }
