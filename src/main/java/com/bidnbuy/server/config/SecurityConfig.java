6cae744a5a89c5f8b09c10d79e1122d9889cea78
simuneu <worksimu@gmail.com>
2025-09-30 17:32:39 +0900

    feat:spring security적용

--- src/main/java/com/bidnbuy/server/config/SecurityConfig.java
+++ src/main/java/com/bidnbuy/server/config/SecurityConfig.java
@@ -1,8 +1,10 @@
 package com.bidnbuy.server.config;
 
+import com.bidnbuy.server.security.CustomAuthenticationEntryPoint;
 import com.bidnbuy.server.security.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -13,13 +15,19 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 @Slf4j
 public class SecurityConfig {
+    @Autowired
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    @Autowired
+    private CustomAuthenticationEntryPoint customAuthenticationEntryPoint;
 
     @Bean
     public PasswordEncoder passwordEncoder(){
@@ -34,15 +42,20 @@ public class SecurityConfig {
             .sessionManagement(session -> session
                     .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
             )
-
+            .exceptionHandling(exceptionHandling
+                    ->exceptionHandling.authenticationEntryPoint(customAuthenticationEntryPoint))
+                
+                //인증 경로 설정
             .authorizeHttpRequests(authorize -> authorize
-                    .requestMatchers("/auth/**").permitAll()
-                    .anyRequest().authenticated()
+                    .requestMatchers("/auth/signup", "/auth/login").permitAll() //인증없이 허용
+                    .anyRequest().authenticated() //그 외 인증 필요
             );
         http.addFilterBefore(
                 jwtAuthenticationFilter,
                 UsernamePasswordAuthenticationFilter.class
         );
+        //요청마다 jwtAuthenticationFilter필터 실행하기
+//        http.addFilterAfter(jwtAuthenticationFilter, CorsFilter.class);
         return http.build();
     }
 }
---
+++ src/main/java/com/bidnbuy/server/security/CustomAuthenticationEntryPoint.java
@@ -0,0 +1,29 @@
+package com.bidnbuy.server.security;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Slf4j
+@Component
+public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response,
+                         AuthenticationException authException) throws IOException{
+        log.error("인증되지 않은 사용자 접근(unauthorized):{}", authException.getMessage());
+
+        response.setContentType("application/json;charset=UTF-8");
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+
+        String jsonResponse = String.format(
+                "{ \"error\": \"Unauthorized\", \"message\": \"%s\" }",
+                authException.getMessage() != null ? authException.getMessage() : "인증 정보가 누락되었거나 유효하지 않습니다."
+        );
+        response.getWriter().write(jsonResponse);
+    }
+}
