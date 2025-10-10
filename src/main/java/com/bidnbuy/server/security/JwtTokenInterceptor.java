e3db96d4875e36b4bc17d3cffe77a9120943a8b3
simuneu <worksimu@gmail.com>
2025-10-10 18:10:39 +0900

    feat:채팅 메세지 전송, fix:채팅방 생성 - 대화가능한 옥션상품 종류 수정

--- src/main/java/com/bidnbuy/server/security/JwtTokenInterceptor.java
+++ src/main/java/com/bidnbuy/server/security/JwtTokenInterceptor.java
@@ -1,20 +1,25 @@
 package com.bidnbuy.server.security;
 
+import io.jsonwebtoken.Claims;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.messaging.simp.stomp.StompCommand;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.messaging.support.MessageHeaderAccessor;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Component;
 
 import java.util.Collections;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class JwtTokenInterceptor implements ChannelInterceptor {
@@ -22,35 +27,24 @@ public class JwtTokenInterceptor implements ChannelInterceptor {
 
     @Override
     public Message<?> preSend(Message<?> message, MessageChannel channel){
-        StompHeaderAccessor accessor = StompHeaderAccessor.wrap(message);
+        StompHeaderAccessor accessor = MessageHeaderAccessor.getAccessor(message, StompHeaderAccessor.class);
 
-        if(StompCommand.CONNECT.equals(accessor.getCommand())){
+        if(accessor !=null && StompCommand.CONNECT.equals(accessor.getCommand())){
             String authToken = accessor.getFirstNativeHeader("Auth-Token");
 
             if(authToken != null && jwtProvider.validateToken(authToken)){
-                try{
-                    final Long userIdLong = jwtProvider.getUserIdFromToken(authToken);
-                    if(userIdLong == null){
-                        throw new AccessDeniedException("토큰에서 유효한 사용자 id를 추출할 수 없습니다.");
-                    }
-                    Authentication authentication = new UsernamePasswordAuthenticationToken(
-                            userIdLong,
-                            null,
-                            Collections.emptyList()
-                    );
-                    accessor.setUser(authentication);
+                Claims claims = jwtProvider.getClaims(authToken);
+                String username = claims.getSubject();
 
-                    accessor.getSessionAttributes().put(
-                            SimpMessageHeaderAccessor.USER_HEADER,
-                            authentication
-                    );
-//                    SecurityContextHolder.getContext().setAuthentication(authentication);
-                } catch (Exception e) {
-                    throw new AccessDeniedException("JWT 처리 중 오류 발생: " + e.getMessage());
-                }
-            }else{
-                System.err.println("STOMP CONNECT 거부: 유효하지 않은 Auth-Token입니다. [Token: " + (authToken != null ? authToken : "NULL") + "]");
-                throw new AccessDeniedException("없거나 유효하지 않은 토큰");
+                User user = new User(username, "", Collections.emptyList());
+                UsernamePasswordAuthenticationToken authenticationToken =
+                        new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+
+                    accessor.setUser(authenticationToken);
+
+                log.info("STOMP CONNECT 인증 성공: {}", username);
+            } else {
+                log.warn("STOMP CONNECT 인증 실패: 잘못된 토큰");
             }
         }
         return message;
